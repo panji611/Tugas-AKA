d9caa1d570dae979dc864c40e2297ccc6e0d295c
panji611 <pratamapanji06@gmail.com>
2024-12-22 23:27:33 +0700

    Update Film.cpp

--- AKA/Film/Film.cpp
+++ AKA/Film/Film.cpp
@@ -1,97 +1,63 @@
 #include "Film.h"
-
-
-void inputAngka(int &NUM, int x){
-    NUM = 0;
+void createList(FilmList &L) {
+    L.first = NULL;
 }
 
-void tambah(int &NUM, int x){
-    NUM = NUM + 1;
-}
-
-void printHasil(int NUM){
-    cout << "Nilai saat ini: " << NUM << endl;
-}
-
-int selectionMenu(){
-    cout << "===== MENU ======" << endl;
-    cout << "1. Menambah N data baru" << endl;
-    cout << "2. Menampilkan semua data" << endl;
-    cout << "3. Nilai terkecil" << endl;
-    cout << "4. Insert middle " << endl;
-    cout << "0. Exit" << endl;
-    cout << "Pilihan menu" << endl;
-
-    int input = 0;
-    cin >> input;
-
-    return input;
-}
-
-void createListList &L){
-    first(L) = nil;
-}
-
-
-address createNew(infotype x){
-
-    address p = new elmlist;
-    info(p) = x;
-    next(p) = nil;
-
+Film* createFilm(string title, string genre, double rating) {
+    Film* p = new Film;
+    p->title = title;
+    p->genre = genre;
+    p->rating = rating;
+    p->next = NULL;
     return p;
 }
 
-
-void insertLast(List &L, address p){
-    if (first(L) == nil){
-        first(L) = p;
-    } else{
-        address q = first(L);
-        while (next(q) != nil) {
-            q = next(q);
-        }
-        next(q) = p;
-    }
-}
-
-address findMin(List L){
-    if (first(L) == nil) return nil;
-    address minNode = first(L);
-    address current = next(first(L));
-
-    while (current != nil) {
-        if (info(current) < info(minNode)) {
-            minNode = current;
-        }
-        current = next(current);
-    }
-    return minNode;
-};
-
-void insertMiddle(List &L, int value){
-    address p = createNew_103012300497(value);
-    if (first(L) == nil) {
-        first(L) = p;
-    } else if (info(first(L)) >= value) {
-        next(p) = first(L);
-        first(L) = p;
+void insertLast(FilmList &L, Film* p) {
+    if (L.first == NULL) {
+        L.first = p;
     } else {
-        address q = first(L);
-        while (next(q) != nil && info(next(q)) < value) {
-            q = next(q);
+        Film* q = L.first;
+        while (q->next != NULL) {
+            q = q->next;
         }
-
-        next(p) = next(q);
-        next(q) = p;
+        q->next = p;
     }
 }
 
-void show(List L){
-    address p = first(L);
-    while (p != nil) {
-        cout << info(p) << ", ";
-        p = next(p);
+void showFilms(FilmList L) {
+    Film* p = L.first;
+    while (p != NULL) {
+        cout << "Title: " << p->title << ", Genre: " << p->genre << ", Rating: " << p->rating << endl;
+        p = p->next;
     }
-    cout << endl;
+}
+
+Film* findBestFilm(FilmList L) {
+    if (L.first == NULL) {
+        return NULL;
+    }
+    Film* best = L.first;
+    Film* p = L.first->next;
+
+    while (p != NULL) {
+        if (p->rating > best->rating) {
+            best = p;
+        }
+        p = p->next;
+    }
+    return best;
+}
+
+void insertSorted(FilmList &L, Film* p) {
+    if (L.first == NULL || L.first->rating <= p->rating) {
+        p->next = L.first;
+        L.first = p;
+    } else {
+        Film* q = L.first;
+        while (q->next != NULL && q->next->rating > p->rating) {
+            q = q->next;
+        }
+        p->next = q->next;
+        q->next = p;
+    }
 }
